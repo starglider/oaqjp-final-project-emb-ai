c2bb82f47910753ab501a8a27296c10ca4c729a1
Luiz Marques <stg-github@stgsys.com>
2026-06-09 20:53:43 -0400

    added testing

---
+++ test_emotion_detection.py
@@ -0,0 +1,9 @@
+from emotion_detection import emotion_detector
+
+tests = [
+['I am glad this happened','joy'],    ['I am really mad about this','anger'],     ['I feel disgusted just hearing about this','disgust'],     ['I am so sad about this','sadness'],     ['I am really afraid that this will happen','fear']]            
+
+
+def test_joy():
+    for test in tests:
+        assert(emotion_detector(test[0])['dominant_emotion']==test[1])
